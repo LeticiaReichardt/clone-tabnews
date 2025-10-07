cf0399e2e412be1573072350f1cef49093033a90
LeticiaReichardt <55117863+LeticiaReichardt@users.noreply.github.com>
2025-10-07 20:49:03 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,12 @@
+import database from "../../../../infra/database.js";
 // function status(request, response) {
 //   response.status(200).send("Deu certo!");
 // }
 
-function status(request, response) {
+async function status(request, response) {
+  console.log("Request at", new Date(), request.headers["user-agent"]);
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response.status(200).json({ chave: "Deu certo!" });
 }
 
