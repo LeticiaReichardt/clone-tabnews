06324b0f5ad90b332299e664cbdc4ee3d3a6920a
LeticiaReichardt <55117863+LeticiaReichardt@users.noreply.github.com>
2026-02-27 19:08:27 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,13 +1,30 @@
 import database from "infra/database.js";
-// function status(request, response) {
-//   response.status(200).send("Deu certo!");
-// }
 
 async function status(request, response) {
-  console.log("Request at", new Date(), request.headers["user-agent"]);
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Deu certo!" });
+  const updateAt = new Date().toISOString();
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsvalue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsvalue),
+        opened_connections: databaseOpenConnectionsResult.rows[0].count,
+      },
+    },
+  });
 }
 
 export default status;
--- testes/integration/api/v1/status/get.test.js
+++ testes/integration/api/v1/status/get.test.js
@@ -1,4 +1,16 @@
 test("GET to /api/v1/status shold retuen 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+  const responseBody = await response.json();
+  const parsedUpedatedAt = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(parsedUpedatedAt);
+  expect(responseBody.dependencies.database.version).toEqual("16.11");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
+
+// test.only("injection", async () => {
+//   const response = await fetch(
+//     "http://localhost:3000/api/v1/status?databaseName=local_db",
+//   );
+// });
