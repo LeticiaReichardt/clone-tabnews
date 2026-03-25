3e68def5dff0c66a5bb10f5a57505dde747aaa20
Leticia Piaz Reichardt <lehreichardte@gmail.com>
2026-03-25 01:25:40 +0000

    handles database ssl connection

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -4,6 +4,7 @@ async function status(request, response) {
   const updateAt = new Date().toISOString();
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
   const databaseMaxConnectionsResult = await database.query(
     "SHOW max_connections;",
   );
