8d5a44c7485b40d2ed172677eb30cda604145fa5
Leticia Piaz Reichardt <55117863+LeticiaReichardt@users.noreply.github.com>
2026-08-03 19:01:33 +0000

    makes `npm test` more robust with `async-retry` and `orchestrator.js`

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -10,37 +10,9 @@ function checkPostgresReady() {
       return;
     }
 
-    console.log("🟢 Postgres is ready!");
-    //   if (error) {
-    //     console.error(`Error executing pg_isready: ${error.message}`);
-    //     setTimeout(checkPostgresReady, 2000);
-    //     return;
-    //   }
-    // const { Client } = require("pg");
-    // const client = new Client({
-    //   host: process.env.POSTGRES_HOST,
-    //   port: process.env.POSTGRES_PORT,
-    //   user: process.env.POSTGRES_USER,
-    //   password: process.env.POSTGRES_PASSWORD,
-    //   database: process.env.POSTGRES_DB,
-    // });
-
-    // console.log("🔴 Waiting for Postgres to be ready...");
-
-    // return client
-    //   .connect()
-    //   .then(() => {
-    //     console.log("🟢 Postgres is ready!");
-    //     client.end();
-    //   })
-    //   .catch((err) => {
-    //     console.log("🔴 Postgres is not ready yet. Retrying in 2 seconds...");
-    //     setTimeout(checkPostgresReady, 2000);
-    //   });
+    console.log("\n🟢 Postgres is ready!");
   }
 }
 
-process.stdout.write(
-  "\n\n🔴 Postgres is not ready yet. Retrying in 2 seconds...",
-);
+process.stdout.write("\n\n🔴 Postgres is not ready yet.");
 checkPostgresReady();
